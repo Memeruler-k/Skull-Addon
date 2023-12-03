cd4f68e82bc3777cd9d4e5d8b3a79def31367ffe
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 15:25:38 +0200

    Create ForceOP.java

---
+++ src/main/java/com/example/addon/modules/ForceOP.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class ForceOP extends Module {
+    public ForceOP() {
+        super(Addon.CATEGORY, "ForceOP", "AuthMeCracker can be used to crack server logins or get Operator.");
+    }
+}

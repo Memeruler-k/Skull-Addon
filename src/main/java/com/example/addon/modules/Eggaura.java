4b757c7e606a8a792c10a2be8984b65c0dc8bcd4
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 15:53:19 +0200

    Create Eggaura.java

---
+++ src/main/java/com/example/addon/modules/Eggaura.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class Eggaura extends Module {
+    public Eggaura() {
+        super(Addon.CATEGORY, "Egg Aura", "Automatically eggs players in a spefic radius");
+    }
+}

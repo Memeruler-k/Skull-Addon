e57d262452c75a899115a446309e5926e6e24569
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 15:57:12 +0200

    Create Messageaura.java

---
+++ src/main/java/com/example/addon/modules/Messageaura.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class Messageaura extends Module {
+    public Messageaura() {
+        super(Addon.CATEGORY, "Message Aura", "Messages players in a spefic radius.");
+    }
+}

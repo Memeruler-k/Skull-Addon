8f6a2fe8042cfccb5554d009682d07b2bb856b28
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 15:29:09 +0200

    Create PacketReach.java

---
+++ src/main/java/com/example/addon/modules/PacketReach.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class PacketReach extends Module {
+    public PacketReach() {
+        super(Addon.CATEGORY, "Packet Reach", "Reach players using packets.");
+    }
+}

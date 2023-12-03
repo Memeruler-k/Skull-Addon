b23ff3f70be7ac9064829885d62b39043169c061
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 14:45:58 +0200

    Create NoCheatPlusCrash.java

---
+++ src/main/java/com/example/addon/modules/NoCheatPlusCrash.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class NoCheatPlusCrash extends Module {
+    public NoCheatPlusCrash() {
+        super(Addon.CATEGORY, "NoCheatPlus Crash", "Attempts to crash the server with NoCheatPlus exploits or vulnerabilities.");
+    }
+}

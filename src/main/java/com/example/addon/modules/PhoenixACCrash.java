cccc8f6c50e314600e7925fd506d731777c6cc88
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 15:06:42 +0200

    Create PhoenixACCrash.java

---
+++ src/main/java/com/example/addon/modules/PhoenixACCrash.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class PhoenixACCrash extends Module {
+    public PhoenixACCrash() {
+        super(Addon.CATEGORY, "PhoenixAC Crash", "Attempts to crash the server using PhoenixAC exploits or vulnerabilities.");
+    }
+}

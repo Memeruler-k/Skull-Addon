41bcd0740ed6f6294cb5da5428dcd972abc46fe7
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 15:23:26 +0200

    Create UnknownPacketCrashV2.java

---
+++ src/main/java/com/example/addon/modules/UnknownPacketCrashV2.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class UnknownPacketCrashV2 extends Module {
+    public UnknownPacketCrashV2() {
+        super(Addon.CATEGORY, "Unknown Packet Crash V2", "More Powerful Unknown Packet Crash Method.");
+    }
+}

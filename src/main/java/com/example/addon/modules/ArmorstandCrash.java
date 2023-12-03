70b3f665660a1ebb260428ebcefacc0bc9309dbb
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 14:43:30 +0200

    Create ArmorstandCrash.java

---
+++ src/main/java/com/example/addon/modules/ArmorstandCrash.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class ArmorstandCrash extends Module {
+    public ArmorstandCrash() {
+        super(Addon.CATEGORY, "Armor Stand Crash", "Attempts to crash the server using Armor stands.");
+    }
+}

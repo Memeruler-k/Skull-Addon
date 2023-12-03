ecca16646d311dedfa51301fdd5b91213dea0fde
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 15:30:01 +0200

    Create ConsoleFlood.java

---
+++ src/main/java/com/example/addon/modules/ConsoleFlood.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class ConsoleFlood extends Module {
+    public ConsoleFlood() {
+        super(Addon.CATEGORY, "Console Flood", "Floods the server console in many different ways.");
+    }
+}

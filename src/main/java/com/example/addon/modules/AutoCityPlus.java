c3424e80f7cf1f3c812ce02207b1aa8a686ed296
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 15:49:38 +0200

    Create AutoCityPlus.java

---
+++ src/main/java/com/example/addon/modules/AutoCityPlus.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class AutoCityPlus extends Module {
+    public AutoCityPlus() {
+        super(Addon.CATEGORY, "Auto City Plus", "idk what this shit does.");
+    }
+}

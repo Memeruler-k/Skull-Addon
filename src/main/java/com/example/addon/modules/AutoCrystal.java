3183cb49327d2041b5f2fa9bc698110007028651
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 15:08:51 +0200

    Create AutoCrystal.java

---
+++ src/main/java/com/example/addon/modules/AutoCrystal.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class Autocrystal extends Module {
+    public AutoCrystal() {
+        super(Addon.CATEGORY, "AutoCrystal", "Automatically crystals players.");
+    }
+}

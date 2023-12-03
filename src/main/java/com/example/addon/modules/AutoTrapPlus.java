4e2994d8d9a3b37222c9067896eaeaa0040d060e
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 15:50:32 +0200

    Create AutoTrapPlus.java

---
+++ src/main/java/com/example/addon/modules/AutoTrapPlus.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class AutoTrapPlus extends Module {
+    public AutoTrapPlus() {
+        super(Addon.CATEGORY, "Auto Trap Plus", "Automatically traps players.");
+    }
+}

8585628e3bf0783210c6afc0e22944ef81c7bd87
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 15:15:16 +0200

    Create MinecartCrash.java

---
+++ src/main/java/com/example/addon/modules/MinecartCrash.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class MinecartCrash extends Module {
+    public MinecartCrash() {
+        super(Addon.CATEGORY, "Minecart Crash", "Attempts to crash the server using Minecarts.");
+    }
+}

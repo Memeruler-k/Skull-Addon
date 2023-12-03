6c6205f233cc8dc06e2840b1b7e43f64e47e949b
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 15:10:30 +0200

    Create LagReducer.java

---
+++ src/main/java/com/example/addon/modules/LagReducer.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class LagReducer extends Module {
+    public LagReducer() {
+        super(Addon.CATEGORY, "LagReducer", "Reduces lag.");
+    }
+}

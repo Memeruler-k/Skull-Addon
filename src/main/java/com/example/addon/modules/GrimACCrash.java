69e182b63e798c9544feac55671b7e65a52ca013
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 14:47:01 +0200

    Create GrimACCrash.java

---
+++ src/main/java/com/example/addon/modules/GrimACCrash.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class GrimACCrash extends Module {
+    public GrimACCrash() {
+        super(Addon.CATEGORY, "GrimAC Crash", "Attempts to crash the server using GrimAC exploits or vulnerabilities.");
+    }
+}

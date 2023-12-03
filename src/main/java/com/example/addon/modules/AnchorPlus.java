88d6c0bd2505de5b2677890bfa0d38466ca3e949
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 15:51:15 +0200

    Create AnchorPlus.java

---
+++ src/main/java/com/example/addon/modules/AnchorPlus.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class AnchorPlus extends Module {
+    public AnchorPlus() {
+        super(Addon.CATEGORY, "Anchor Plus", "Anchor aura but Better.");
+    }
+}

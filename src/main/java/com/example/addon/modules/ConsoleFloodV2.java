e531256455afdb43e1bb397bce2fee9a2675e675
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 15:30:57 +0200

    Create ConsoleFloodV2.java

---
+++ src/main/java/com/example/addon/modules/ConsoleFloodV2.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class ConsoleFloodV2 extends Module {
+    public ConsoleFloodV2() {
+        super(Addon.CATEGORY, "Console Flood V2", "Same as the first version but more powerful and configurable.");
+    }
+}

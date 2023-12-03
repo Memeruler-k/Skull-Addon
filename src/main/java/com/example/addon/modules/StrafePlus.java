5d63554b88ffe0ed23ac58ad44b91ece974223f5
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 14:40:51 +0200

    Update StrafePlus.java

--- src/main/java/com/example/addon/modules/StrafePlus.java
+++ src/main/java/com/example/addon/modules/StrafePlus.java
@@ -3,8 +3,8 @@ package com.example.addon.modules;
 import com.example.addon.Addon;
 import meteordevelopment.meteorclient.systems.modules.Module;
 
-public class ModuleExample extends Module {
-    public ModuleExample() {
+public class StrafePlus extends Module {
+    public StrafePlus() {
         super(Addon.CATEGORY, "StrafePlus", "Bypass Strafe for certain AntiCheat or Gamemodes"
     }
 }

24fb835e968cca7bba01b53540562b63b1ef138e
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 15:14:20 +0200

    Create MinecartBomb.java

---
+++ src/main/java/com/example/addon/modules/MinecartBomb.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class MinecartBomb extends Module {
+    public MinecartBomb() {
+        super(Addon.CATEGORY, "Minecart Bomb", "Explodes Minecarts.");
+    }
+}

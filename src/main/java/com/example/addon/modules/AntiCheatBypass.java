2fdb2951d0f0b60f5c9977e491f443d694b1fe8b
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 15:26:32 +0200

    Create AntiCheatBypass.java

---
+++ src/main/java/com/example/addon/modules/AntiCheatBypass.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class AntiCheatBypass extends Module {
+    public AntiCheatBypass() {
+        super(Addon.CATEGORY, "Anti Cheat Bypass", "Can be used to Bypass certain Anti Cheats.");
+    }
+}

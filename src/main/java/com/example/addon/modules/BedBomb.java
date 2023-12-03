6d3bc14a0121bb3b69d8b7afff9856e9222e76cd
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 15:12:58 +0200

    Create BedBomb.java

---
+++ src/main/java/com/example/addon/modules/BedBomb.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class BedBomb extends Module {
+    public BedBomb() {
+        super(Addon.CATEGORY, "Bed Bomb", "Explodes beds. NEED TO BE IN THE NETHER WITH A BED.");
+    }
+}

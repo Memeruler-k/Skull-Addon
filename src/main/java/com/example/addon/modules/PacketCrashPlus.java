1b011e8e58da8aaf303c07f2288e2a129496d841
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 15:21:09 +0200

    Create PacketCrashPlus.java

---
+++ src/main/java/com/example/addon/modules/PacketCrashPlus.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class PacketCrashPlus extends Module {
+    public PacketCrashPlus() {
+        super(Addon.CATEGORY, "Packet Crash+", "More Powerful and Configurable Server crash method.");
+    }
+}

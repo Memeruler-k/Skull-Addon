6016758518a59169e45d11cb5ffedcc26617ca03
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-03 15:27:29 +0200

    Create PacketFly.java

---
+++ src/main/java/com/example/addon/modules/PacketFly.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class PacketFly extends Module {
+    public PacketFly() {
+        super(Addon.CATEGORY, "Packet Fly", "Fly using packets.");
+    }
+}

9b0b9f6afff0577c88fea126872a71f511dd266b
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-04 15:49:43 +0200

    Create AutoMeow.java

---
+++ src/main/java/com/example/addon/modules/AutoMeow.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class AutoMeow extends Module {
+    public AutoMeow() {
+        super(Addon.CATEGORY, "Auto Meow", "Automatically meows.");
+    }
+}

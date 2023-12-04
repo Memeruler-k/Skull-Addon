19a4c61edaff2db8d54481bb0725c32fb3b4cbec
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-04 15:39:07 +0200

    Create NameProtection.java

---
+++ src/main/java/com/example/addon/modules/NameProtection.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class NameProtection extends Module {
+    public NameProtection() {
+        super(Addon.CATEGORY, "Name Protection", "Protects your Username by spoofing it.");
+    }
+}

94f9ad8cc7a6de71adca729d08c55a696ba9b1cf
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-04 15:30:49 +0200

    Create CombatLog.java

---
+++ src/main/java/com/example/addon/modules/CombatLog.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class CombatLog extends Module {
+    public CombatLog() {
+        super(Addon.CATEGORY, "Combat Log", "Combat Log for servers.");
+    }
+}

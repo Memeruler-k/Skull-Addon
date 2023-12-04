d53e40a52541428c54ea57c6ea8f87ef0aa786c3
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-04 15:38:08 +0200

    Create SkinProtection.java

---
+++ src/main/java/com/example/addon/modules/SkinProtection.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class SkinProtection extends Module {
+    public SkinProtection() {
+        super(Addon.CATEGORY, "Skin Protection", "Protects your skin by setting another skin.");
+    }
+}

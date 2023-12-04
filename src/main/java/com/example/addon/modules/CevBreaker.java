49ce1d808eb069e5fb3c2c109f45116fbdb2083d
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-04 15:29:08 +0200

    Create CevBreaker.java

---
+++ src/main/java/com/example/addon/modules/CevBreaker.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class CevBreaker extends Module {
+    public CevBreaker() {
+        super(Addon.CATEGORY, "Cev Breaker", "idk what this shit does ");
+    }
+}

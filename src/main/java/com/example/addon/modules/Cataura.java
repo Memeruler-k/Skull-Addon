e9f4bebc691816d08c9570e1a1dee9eda183e6b0
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-04 15:40:18 +0200

    Create Cataura.java

---
+++ src/main/java/com/example/addon/modules/Cataura.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class Cataura extends Module {
+    public Cataura() {
+        super(Addon.CATEGORY, "Cat Aura", "CREATIVE MODE REQUIRED. Spawns cats nearby other players.");
+    }
+}

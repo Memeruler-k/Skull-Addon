b3e2832b4abcbf9a72f4a5315a879f12849a66d0
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-04 15:43:32 +0200

    Create BedDisabler.java

---
+++ src/main/java/com/example/addon/modules/BedDisabler.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class BedDisabler extends Module {
+    public BedDisabler() {
+        super(Addon.CATEGORY, "Bed Disabler", "Disables beds.");
+    }
+}

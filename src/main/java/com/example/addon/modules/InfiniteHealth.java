6175a072fa5b51512a646233dbf70f3816d3d9fb
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-04 15:53:52 +0200

    Create InfiniteHealth.java

---
+++ src/main/java/com/example/addon/modules/InfiniteHealth.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class InfiniteHealth extends Module {
+    public InfiniteHealth() {
+        super(Addon.CATEGORY, "Infinite Health", "ONLY WORKS ON 1.8-1.16.x servers.");
+    }
+}

a986986c3d1a82184fc42ff21c833191a8a63bbf
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-04 15:51:32 +0200

    Create InfiniteChat.java

---
+++ src/main/java/com/example/addon/modules/InfiniteChat.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class InfiniteChat extends Module {
+    public InfiniteChat() {
+        super(Addon.CATEGORY, "Infinite Chat", "Makes your chat history to infinite so you dont lose any messages.");
+    }
+}

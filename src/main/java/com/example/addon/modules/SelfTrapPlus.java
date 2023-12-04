37778e73d41c40c756dbc367fc6350b277100efb
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-04 15:46:44 +0200

    Create SelfTrapPlus.java

---
+++ src/main/java/com/example/addon/modules/SelfTrapPlus.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class SelfTrapPlus extends Module {
+    public SelfTrapPlus() {
+        super(Addon.CATEGORY, "Self Trap Plus", "Traps you automatically in many different ways.");
+    }
+}

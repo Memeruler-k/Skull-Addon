69873b7a29ffc743b4c663f7b3013b483d403f5c
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-04 15:44:53 +0200

    Create NameTagsPlus.java

---
+++ src/main/java/com/example/addon/modules/NameTagsPlus.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class NameTagsPlus extends Module {
+    public NameTagsPlus() {
+        super(Addon.CATEGORY, "NameTags Plus", "NameTags but better now can see Players IP,ISP,Address and Location!");
+    }
+}

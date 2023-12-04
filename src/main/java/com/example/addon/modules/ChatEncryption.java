a6b1c8fe2459571ba82aeb34996ef680ef2550c1
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-04 15:50:40 +0200

    Create ChatEncryption.java

---
+++ src/main/java/com/example/addon/modules/ChatEncryption.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class ChatEncryption extends Module {
+    public ChatEncryption() {
+        super(Addon.CATEGORY, "Chat Encryption", "Encrypts your chat.");
+    }
+}

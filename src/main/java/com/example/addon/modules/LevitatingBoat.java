100a41c620b2c2bb4101554869245cff66e613db
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:21:10 +0200

    Create LevitatingBoat.java

---
+++ src/main/java/com/example/addon/modules/LevitatingBoat.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class LevitatingBoat extends Module {
+    public LevitatingBoat() {
+        super(Addon.CATEGORY, "Levitating Boat", "Makes your boat levitate in air.");
+    }
+}

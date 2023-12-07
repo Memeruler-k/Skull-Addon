45d4e7cafba55341eb5e57a2d1cecf29e99f06a9
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:59:00 +0200

    Create JigsawCrash.java

---
+++ src/main/java/com/example/addon/modules/JigsawCrash.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class JigsawCrash extends Module {
+    public JigsawCrash() {
+        super(Addon.CATEGORY, "Jigsaw Crash", "idk how to explain this.");
+    }
+}

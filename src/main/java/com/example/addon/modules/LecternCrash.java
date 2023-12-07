c19c742dc6cff4bd323fd357b6a9fb6c04f0dcb3
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:28:36 +0200

    Create LecternCrash.java

---
+++ src/main/java/com/example/addon/modules/LecternCrash.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class LecternCrash extends Module {
+    public LecternCrash() {
+        super(Addon.CATEGORY, "Lectern Crash", "Attempts to crash the server using Lectern Exploits or Glitches.");
+    }
+}

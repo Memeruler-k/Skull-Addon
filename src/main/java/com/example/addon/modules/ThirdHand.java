ad45b7689786f15e8455dfb7c496ef8a0a10d3a0
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:19:52 +0200

    Create ThirdHand.java

---
+++ src/main/java/com/example/addon/modules/ThirdHand.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class ThirdHand extends Module {
+    public ThirdHand() {
+        super(Addon.CATEGORY, "Third Hand", "Makes you have a third hand.");
+    }
+}

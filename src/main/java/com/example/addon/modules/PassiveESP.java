9b45e6ad48e3099cdb84ea416b0c5efb16f8753e
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:18:11 +0200

    Create PassiveESP.java

---
+++ src/main/java/com/example/addon/modules/PassiveESP.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class PassiveESP extends Module {
+    public PassiveESP() {
+        super(Addon.CATEGORY, "Passive ESP", "Displays a Box or ESP on Passive Mobs or Players");
+    }
+}

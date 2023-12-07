35d228cf8a0dce5d549f34ededd4d59e3a4e756d
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:22:11 +0200

    Create ScoreboardPlus.java

---
+++ src/main/java/com/example/addon/modules/ScoreboardPlus.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class ScoreboardPlus extends Module {
+    public ScoreboardPlus() {
+        super(Addon.CATEGORY, "Scoreboard Plus", "CLIENT-SIDE. Tweaks and Improves the Scoreboard.");
+    }
+}

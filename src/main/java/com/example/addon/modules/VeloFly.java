0a6f05bb81f7bbfe8fb985a5830dccf4f6e63fd7
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:24:55 +0200

    Create VeloFly.java

---
+++ src/main/java/com/example/addon/modules/VeloFly.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class VeloFly extends Module {
+    public VeloFly() {
+        super(Addon.CATEGORY, "Velo Fly", "Velocity Fly.");
+    }
+}

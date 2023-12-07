d043abfd2bfc59cb98fed22b9c63eb4ebef73ee7
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:07:09 +0200

    Create ServerSpoof.java

---
+++ src/main/java/com/example/addon/modules/ServerSpoof.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class ServerSpoof extends Module {
+    public ServerSpoof() {
+        super(Addon.CATEGORY, "Server Spoof", "Spoof servers.");
+    }
+}

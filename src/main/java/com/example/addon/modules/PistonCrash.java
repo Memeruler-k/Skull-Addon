b99e99604a0c20c64e31209fe95639d37d9eb81e
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:29:32 +0200

    Create PistonCrash.java

---
+++ src/main/java/com/example/addon/modules/PistonCrash.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class PistonCrash extends Module {
+    public PistonCrash() {
+        super(Addon.CATEGORY, "Piston Crash", "Attempts to crash the server using Piston Exploits or Glitches.");
+    }
+}

0ffa0b4e4aa9206e1444b79a47eb57c3e23549a3
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:49:08 +0200

    Create BreakESP.java

---
+++ src/main/java/com/example/addon/modules/BreakESP.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class BreakESP extends Module {
+    public BreakESP() {
+        super(Addon.CATEGORY, "Break ESP", "idk what this shit is supposed to do.");
+    }
+}

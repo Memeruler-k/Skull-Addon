5158e36e7182cc2d913a2bdfaeca4805b2be610e
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:19:07 +0200

    Create HandManager.java

---
+++ src/main/java/com/example/addon/modules/HandManager.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class HandManager extends Module {
+    public HandManager() {
+        super(Addon.CATEGORY, "Hand Manager", "Manage your hand.");
+    }
+}

4ccb6b9832fdca2259a79a9e7cda5b4f804f76d9
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:27:34 +0200

    Create BuildRandom.java

---
+++ src/main/java/com/example/addon/modules/BuildRandom.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class RandomBuild extends Module {
+    public RandomBuild() {
+        super(Addon.CATEGORY, "Build Random", "Builds Random stuff.");
+    }
+}

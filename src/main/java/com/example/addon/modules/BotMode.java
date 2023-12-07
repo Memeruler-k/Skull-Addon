b1ac2e7a679846fa6cc22db988516d1c9006fe3d
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:17:07 +0200

    Create BotMode.java

---
+++ src/main/java/com/example/addon/modules/BotMode.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class BotMode extends Module {
+    public BotMode() {
+        super(Addon.CATEGORY, "Bot Mode", "Makes a Bot/AI Fight or play the game for you.");
+    }
+}

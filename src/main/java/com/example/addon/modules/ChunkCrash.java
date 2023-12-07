1aa96474b408224b8426c82b97823097a89c9f70
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:23:23 +0200

    Create ChunkCrash.java

---
+++ src/main/java/com/example/addon/modules/ChunkCrash.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class ChunkCrash extends Module {
+    public ChunkCrash() {
+        super(Addon.CATEGORY, "Chunk Crash", "Attempts to crash the server using Chunk Exploits or Glitches.");
+    }
+}

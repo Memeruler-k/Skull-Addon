2130bb559392e54c4a5f051f7f4879f19bf24687
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:55:19 +0200

    Create XsDupe.java

---
+++ src/main/java/com/example/addon/modules/XsDupe.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class XsDupe extends Module {
+    public XsDupe() {
+        super(Addon.CATEGORY, "XS Dupe", "Allows you to dupe any item.");
+    }
+}

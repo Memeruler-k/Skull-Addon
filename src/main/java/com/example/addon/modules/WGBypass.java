6d1f2e7c692fc43b31c2dda62d3f16660abd9ebe
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:10:22 +0200

    Create WGBypass.java

---
+++ src/main/java/com/example/addon/modules/WGBypass.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class WGBypass extends Module {
+    public WGBypass() {
+        super(Addon.CATEGORY, "WGBypass", "Bypass World Guard.");
+    }
+}

e08c885d1fa07b07bbc02c3ec33624499c78f0bb
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:49:56 +0200

    Create BetterAutoLog.java

---
+++ src/main/java/com/example/addon/modules/BetterAutoLog.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class BetterAutoLog extends Module {
+    public BetterAutoLog() {
+        super(Addon.CATEGORY, "Better Auto Log", "Auto Log but better.");
+    }
+}

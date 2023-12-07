53093b9a5dfc2948ec82cd35fd5c32027bf06715
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:48:26 +0200

    Create TNTaura.java

---
+++ src/main/java/com/example/addon/modules/TNTaura.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class TNTaura extends Module {
+    public TNTaura() {
+        super(Addon.CATEGORY, "TNT Aura", "OP OR CREATIVE REQUIRED. Spawns TNT on nearby players.");
+    }
+}

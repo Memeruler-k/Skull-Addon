bf4b77d7b2e2b63d98698db9541e0f660486e9c8
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:24:13 +0200

    Create AutoExecute.java

---
+++ src/main/java/com/example/addon/modules/AutoExecute.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class AutoExecute extends Module {
+    public AutoExecute() {
+        super(Addon.CATEGORY, "Auto Execute", "Automatically Executes commands.");
+    }
+}

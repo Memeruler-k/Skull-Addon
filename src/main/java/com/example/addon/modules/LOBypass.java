8798c571af4d54f111e27aa1f691e4615664c68c
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-07 16:11:06 +0200

    Create LOBypass.java

---
+++ src/main/java/com/example/addon/modules/LOBypass.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class LOBypass extends Module {
+    public LOBypass() {
+        super(Addon.CATEGORY, "LO Bypass", "Bypass LO.");
+    }
+}

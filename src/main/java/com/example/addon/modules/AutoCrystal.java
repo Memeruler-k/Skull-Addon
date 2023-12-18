f0626133deff8db07ac6142d51871d76be1fed1a
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-18 19:35:02 +0200

    Update AutoCrystal.java

--- src/main/java/com/example/addon/modules/AutoCrystal.java
+++ src/main/java/com/example/addon/modules/AutoCrystal.java
@@ -3,8 +3,8 @@ package com.example.addon.modules;
 import com.example.addon.Addon;
 import meteordevelopment.meteorclient.systems.modules.Module;
 
-public class Autocrystal extends Module {
+public class AutoCrystal extends Module {
     public AutoCrystal() {
-        super(Addon.CATEGORY, "Auto Crystal", "Automatically crystals players.");
+        super(Addon.CATEGORY, "Auto Crystal", "Automatically crystals players in PVP.");
     }
 }

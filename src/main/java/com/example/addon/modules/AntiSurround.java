f19c6b5e3f28871c2c34860ee7ea6b260833c51b
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-18 17:07:59 +0200

    Update AntiSurround.java

--- src/main/java/com/example/addon/modules/AntiSurround.java
+++ src/main/java/com/example/addon/modules/AntiSurround.java
@@ -4,7 +4,7 @@ import com.example.addon.Addon;
 import meteordevelopment.meteorclient.systems.modules.Module;
 
 public class AntiSurround extends Module {
-    public MAntiSurround() {
+    public AntiSurround() {
         super(Addon.CATEGORY, "Anti Surround", "Prevents players from surrounding you or someone else.");
     }
 }

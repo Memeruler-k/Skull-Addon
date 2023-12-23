c36edf9f7c176a994151deb81a6f9d1f75aa373c
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-23 19:54:37 +0200

    Create SkinCrash.java

---
+++ src/main/java/com/example/addon/modules/SkinCrash.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class SkinCrash extends Module {
+    public SkinCrash() {
+        super(Addon.CATEGORY, "Skin Crash", "Attempts to crash the server using Custom Skins.");
+    }
+}

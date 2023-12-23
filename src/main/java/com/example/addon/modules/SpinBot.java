243b0707020c14006e325353c262efef6402055a
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-23 20:02:45 +0200

    Create SpinBot.java

---
+++ src/main/java/com/example/addon/modules/SpinBot.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class SpinBot extends Module {
+    public SpinBot() {
+        super(Addon.CATEGORY, "Spin Bot", "Spins you like crazy. Only works in third person!");
+    }
+}

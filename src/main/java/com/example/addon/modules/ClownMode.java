c6c5b49f228bb7fc392a90b41468625e78a0496f
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-23 20:05:45 +0200

    Create ClownMode.java

---
+++ src/main/java/com/example/addon/modules/ClownMode.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class ClownMode extends Module {
+    public ClownMode() {
+        super(Addon.CATEGORY, "Clown Mode", "Turns you into a Clown.");
+    }
+}

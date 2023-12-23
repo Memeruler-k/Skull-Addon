303948d48f5e96e88855d6022ce905dcf2bde6af
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-23 20:05:07 +0200

    Create StreamerMode.java

---
+++ src/main/java/com/example/addon/modules/StreamerMode.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class StreamerMode extends Module {
+    public StreamerMode() {
+        super(Addon.CATEGORY, "Streamer Mode", "Hides everyones name and skin.");
+    }
+}

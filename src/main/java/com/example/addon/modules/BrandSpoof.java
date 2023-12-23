b6a899ecf3445557f1b41a8b36578927f2766805
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-23 19:59:29 +0200

    Create BrandSpoof.java

---
+++ src/main/java/com/example/addon/modules/BrandSpoof.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class BrandSpoof extends Module {
+    public BrandSpoof() {
+        super(Addon.CATEGORY, "Brand Spoof", "Spoof your client brand.");
+    }
+}

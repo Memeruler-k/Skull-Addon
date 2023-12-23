7999364e0cf3d16b49ee98e2fb8c3154287dfea1
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-23 20:04:23 +0200

    Create NerdMode.java

---
+++ src/main/java/com/example/addon/modules/NerdMode.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class NerdMode extends Module {
+    public NerdMode() {
+        super(Addon.CATEGORY, "Nerd Mode", "Turns you into a nerd.");
+    }
+}

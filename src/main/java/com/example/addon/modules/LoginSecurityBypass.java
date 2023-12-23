b46f30cf6426856a0fbe8a175e23ebbc90cc2c54
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-23 19:57:57 +0200

    Create LoginSecurityBypass.java

---
+++ src/main/java/com/example/addon/modules/LoginSecurityBypass.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class LoginSecurityBypass extends Module {
+    public LoginSecurityBypass() {
+        super(Addon.CATEGORY, "Login Security Bypass", "Bypasses LoginSecurity password system.");
+    }
+}

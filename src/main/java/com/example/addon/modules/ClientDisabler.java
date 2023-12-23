81c8e1d39b5365fdb4f7155185374d7cef9f9168
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-23 20:00:18 +0200

    Create ClientDisabler.java

---
+++ src/main/java/com/example/addon/modules/ClientDisabler.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class ClientDisabler extends Module {
+    public ClientDisabler() {
+        super(Addon.CATEGORY, "Client Disabler", "Disable others hack clients.");
+    }
+}

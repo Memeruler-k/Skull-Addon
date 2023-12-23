62769645a2155a784c927ff93ea41e414c7e7efd
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2023-12-23 20:03:36 +0200

    Create BanEvasion.java

---
+++ src/main/java/com/example/addon/modules/BanEvasion.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class BanEvasion extends Module {
+    public BanEvasion() {
+        super(Addon.CATEGORY, "AAC Crash", "Evades server bans with mutliple accounts.");
+    }
+}

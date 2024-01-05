6d2e48f2ce119198dadac3f915db5682831a41bd
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-01-05 10:43:33 +0200

    Create GarbageAim.java

---
+++ src/main/java/com/example/addon/modules/GarbageAim.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class GarbageAim extends Module {
+    public GarbageAim() {
+        super(Addon.CATEGORY, "Garbage Aim", "Gives you garbage aka bad aim.");
+    }
+}

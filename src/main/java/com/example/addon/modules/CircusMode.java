f5eecf7c288e142a838c98d23befc27fb3615379
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-01-05 10:39:41 +0200

    Create CircusMode.java

---
+++ src/main/java/com/example/addon/modules/CircusMode.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class CircusMode extends Module {
+    public CircusMode() {
+        super(Addon.CATEGORY, "Circus Mode", "Plays circus music in earrape when killing someone.");
+    }
+}

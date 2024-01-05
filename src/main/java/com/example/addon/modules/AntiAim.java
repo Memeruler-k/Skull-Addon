d57074275268fec7262c620e1f9a3b037abd99b8
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-01-05 10:42:46 +0200

    Create AntiAim.java

---
+++ src/main/java/com/example/addon/modules/AntiAim.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class AntiAim extends Module {
+    public AntiAim() {
+        super(Addon.CATEGORY, "Anti Aim", "Fucks your enemies with their aim.");
+    }
+}

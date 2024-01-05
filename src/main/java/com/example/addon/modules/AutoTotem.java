b551fd35080814b7a2e05a428627d5b581000eff
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-01-05 10:40:31 +0200

    Create AutoTotem.java

---
+++ src/main/java/com/example/addon/modules/AutoTotem.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class AutoTotem extends Module {
+    public AutoTotem() {
+        super(Addon.CATEGORY, "Auto Totem", "Automatically puts totem in your second hand.");
+    }
+}

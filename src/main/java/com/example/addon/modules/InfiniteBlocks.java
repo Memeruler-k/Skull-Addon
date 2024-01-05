106bd9b1a5336f6fe2eef75ca006ed1df1a9483a
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-01-05 10:47:11 +0200

    Create InfiniteBlocks.java

---
+++ src/main/java/com/example/addon/modules/InfiniteBlocks.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class InfiniteBlocks extends Module {
+    public InfiniteBlocks() {
+        super(Addon.CATEGORY, "Infinite Blocks", "Attempts to exploit the game to give you infinite blocks.");
+    }
+}

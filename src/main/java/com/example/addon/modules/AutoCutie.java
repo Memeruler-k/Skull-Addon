d11f203bf119ed3bbaea20f4c443095ef01936c4
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-01-05 10:41:15 +0200

    Create AutoCutie.java

---
+++ src/main/java/com/example/addon/modules/AutoCutie.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class AutoCutie extends Module {
+    public AutoCutie() {
+        super(Addon.CATEGORY, "Auto Cutie", "Auto Cutie???.");
+    }
+}

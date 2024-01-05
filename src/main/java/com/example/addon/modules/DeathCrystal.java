2fe99c3fce77f58faedb0c603b3116aa2536bd8e
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-01-05 10:48:36 +0200

    Create DeathCrystal.java

---
+++ src/main/java/com/example/addon/modules/DeathCrystal.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class DeathCrystal extends Module {
+    public DeathCrystal() {
+        super(Addon.CATEGORY, "Death Crystal", "Makes your crystals 1 shot.");
+    }
+}

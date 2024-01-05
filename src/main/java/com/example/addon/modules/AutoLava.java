f6245364e349f1ab8bf4b7818815701ce37e2604
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-01-05 10:42:00 +0200

    Create AutoLava.java

---
+++ src/main/java/com/example/addon/modules/AutoLava.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class AutoLava extends Module {
+    public AutoLava() {
+        super(Addon.CATEGORY, "Auto Lava", "Automatically places lava on your enemies");
+    }
+}

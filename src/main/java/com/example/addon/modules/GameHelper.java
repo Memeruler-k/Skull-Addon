d04a2c6221b989aac7fd26e67e86ac4361a12572
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-01-05 10:45:57 +0200

    Create GameHelper.java

---
+++ src/main/java/com/example/addon/modules/GameHelper.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class GameHelper extends Module {
+    public GameHelper() {
+        super(Addon.CATEGORY, "Game Helper", "Helps you play the game.");
+    }
+}

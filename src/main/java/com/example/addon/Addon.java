70eaa26f27f80bd0194b54405abcb0e974dcfda1
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-01-31 19:38:05 +0200

    Update Addon.java

--- src/main/java/com/example/addon/Addon.java
+++ src/main/java/com/example/addon/Addon.java
@@ -1,11 +1,9 @@
 package com.example.addon;
 
-import com.example.addon.commands.CommandExample;
 import com.example.addon.hud.HudExample;
 import com.example.addon.modules.ModuleExample;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.systems.commands.Commands;
 import meteordevelopment.meteorclient.systems.hud.Hud;
 import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;

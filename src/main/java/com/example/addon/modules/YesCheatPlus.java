b502664e8ab353462e62e2cb518f478c02a0a7df
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-01-31 19:24:37 +0200

    Update YesCheatPlus.java

--- src/main/java/com/example/addon/modules/YesCheatPlus.java
+++ src/main/java/com/example/addon/modules/YesCheatPlus.java
@@ -3,8 +3,8 @@ package com.example.addon.modules;
 import com.example.addon.Addon;
 import meteordevelopment.meteorclient.systems.modules.Module;
 
-public class ModuleExample extends Module {
-    public ModuleExample() {
+public class YesCheatPlus extends Module {
+    public YesCheatPlus() {
         super(Addon.CATEGORY, "YesCheat+", "YesCheat+ can be used to prevent admins and server plugins from detecting your cheats. It can also be used to reduce your chances of getting banned.");
     }
 }

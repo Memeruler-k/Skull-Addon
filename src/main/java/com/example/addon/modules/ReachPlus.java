4f1fccc2ab7c6a6f59ca7f3ab87ad78d5be54559
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-01-31 19:21:55 +0200

    Update ReachPlus.java

--- src/main/java/com/example/addon/modules/ReachPlus.java
+++ src/main/java/com/example/addon/modules/ReachPlus.java
@@ -3,8 +3,8 @@ package com.example.addon.modules;
 import com.example.addon.Addon;
 import meteordevelopment.meteorclient.systems.modules.Module;
 
-public class ModuleExample extends Module {
-    public ModuleExample() {
-        super(Addon.CATEGORY, "ReachPlus", "Bypass Reach for Certain AntiCheats or Gamemodes");
+public class ReachPlus extends Module {
+    public ReachPlus() {
+        super(Addon.CATEGORY, "Reach+", "Bypass Reach for Certain AntiCheats or Gamemodes");
     }
 }

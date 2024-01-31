09b379b61469a07daf129a944363fe9f7be49ae0
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-01-31 19:25:13 +0200

    Update StepPlus.java

--- src/main/java/com/example/addon/modules/StepPlus.java
+++ src/main/java/com/example/addon/modules/StepPlus.java
@@ -3,8 +3,8 @@ package com.example.addon.modules;
 import com.example.addon.Addon;
 import meteordevelopment.meteorclient.systems.modules.Module;
 
-public class ModuleExample extends Module {
-    public ModuleExample() {
-        super(Addon.CATEGORY, "StepPlus", "Bypass Step for Certain AntiCheats or Gamemodes");
+public class StepPlus extends Module {
+    public StepPlus() {
+        super(Addon.CATEGORY, "Step+", "Bypass Step for Certain AntiCheats or Gamemodes");
     }
 }

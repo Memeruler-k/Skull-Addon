a4762f571f8f29d699b2490fd83f23ef486dabda
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-01-31 19:31:28 +0200

    Update PacketCrashV2.java

--- src/main/java/com/example/addon/modules/PacketCrashV2.java
+++ src/main/java/com/example/addon/modules/PacketCrashV2.java
@@ -3,8 +3,8 @@ package com.example.addon.modules;
 import com.example.addon.Addon;
 import meteordevelopment.meteorclient.systems.modules.Module;
 
-public class PacketCrash extends Module {
-    public PacketCrash() {
+public class PacketCrashV2 extends Module {
+    public PacketCrashV2() {
         super(Addon.CATEGORY, "Packet Crash V2", "Attempts to crash the server with many different packets.");
     }
 }

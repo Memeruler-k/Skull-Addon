39abc1a238594ef245a08e94eff66d24beef6a63
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-01-31 19:21:07 +0200

    Update DisablerV2.java

--- src/main/java/com/example/addon/modules/DisablerV2.java
+++ src/main/java/com/example/addon/modules/DisablerV2.java
@@ -3,8 +3,8 @@ package com.example.addon.modules;
 import com.example.addon.Addon;
 import meteordevelopment.meteorclient.systems.modules.Module;
 
-public class ModuleExample extends Module {
-    public ModuleExample() {
-        super(Addon.CATEGORY, "Disabler V2", "Bypass Anti-Cheat");
+public class DisablerV2 extends Module {
+    public DisablerV2() {
+        super(Addon.CATEGORY, "Disabler V2", "Disabler or Bypass anticheats.");
     }
 }

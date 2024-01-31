30a66335675a6213dcfecb6f54ccca57c6aad1d8
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-01-31 18:58:30 +0200

    Update Addon.java

--- src/main/java/com/example/addon/Addon.java
+++ src/main/java/com/example/addon/Addon.java
@@ -19,7 +19,7 @@ public class Addon extends MeteorAddon {
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Addon Template");
+        LOG.info("Initializing Skull Addon");
 
         // Modules
         Modules.get().add(new FlyPlus());

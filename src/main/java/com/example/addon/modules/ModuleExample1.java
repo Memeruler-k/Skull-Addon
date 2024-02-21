1e3751c6ad08b81191b37594f7d9b939aaea5f93
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-02-21 19:21:42 +0200

    Update ModuleExample1.java

--- src/main/java/com/example/addon/modules/ModuleExample1.java
+++ src/main/java/com/example/addon/modules/ModuleExample1.java
@@ -4,7 +4,7 @@ import com.example.addon.Addon;
 import meteordevelopment.meteorclient.systems.modules.Module;
 
 public class ModuleExample1 extends Module {
-    public ModuleExample() {
+    public ModuleExample1() {
         super(Addon.CATEGORY, "AAC Crash", "Supposed crash methrods for servers using AAC");
     }
 }

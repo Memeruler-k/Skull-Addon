36da6267bcf74905ba8b35e568746c3d3ed91f34
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-02-21 16:56:35 +0200

    Update FlyPlus.java

--- src/main/java/com/example/addon/modules/FlyPlus.java
+++ src/main/java/com/example/addon/modules/FlyPlus.java
@@ -3,8 +3,8 @@ package com.example.addon.modules;
 import com.example.addon.Addon;
 import meteordevelopment.meteorclient.systems.modules.Module;
 
-public class FlyPlus extends Module {
-    public FlyPlus() {
+public class ModuleExample extends Module {
+    public ModuleExample() {
         super(Addon.CATEGORY, "Fly Plus", "Bypass Fly for Certain AntiCheats or Gamemodes");
     }
 }

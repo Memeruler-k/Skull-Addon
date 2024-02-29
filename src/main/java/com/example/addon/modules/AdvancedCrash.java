40dbd06b93bcbf87aceaafb898ebeb8ea07db6c0
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-02-29 15:50:41 +0200

    Update AdvancedCrash.java

--- src/main/java/com/example/addon/modules/AdvancedCrash.java
+++ src/main/java/com/example/addon/modules/AdvancedCrash.java
@@ -3,8 +3,8 @@ package com.example.addon.modules;
 import com.example.addon.Addon;
 import meteordevelopment.meteorclient.systems.modules.Module;
 
-public class AdvancedCrash extends Module {
-    public AdvancedCrash() {
+public class Crash extends Module {
+    public Crash() {
         super(Addon.CATEGORY, "Advanced Crash", "A New Developed Server Crash Method");
     }
 }

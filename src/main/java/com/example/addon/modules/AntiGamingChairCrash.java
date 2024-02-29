04a01ced7ba756daaa9561b21e9fae30d791d4dd
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-02-29 15:50:54 +0200

    Update AntiGamingChairCrash.java

--- src/main/java/com/example/addon/modules/AntiGamingChairCrash.java
+++ src/main/java/com/example/addon/modules/AntiGamingChairCrash.java
@@ -3,8 +3,8 @@ package com.example.addon.modules;
 import com.example.addon.Addon;
 import meteordevelopment.meteorclient.systems.modules.Module;
 
-public class AntiGamingChairCrash extends Module {
-    public AntiGamingChairCrash() {
+public class Crash extends Module {
+    public Crash() {
         super(Addon.CATEGORY, "AntiGamingChair Crash", "Attempts to crash the server with AntiGamingChairAC exploits or vulnerabilities.");
     }
 }

da216d513d78d67d7e9bcd348a763c230cda006e
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 18:20:48 +0200

    Update and rename ModuleExample1.java to AACCrash.java

--- src/main/java/com/example/addon/modules/AACCrash.java
+++ src/main/java/com/example/addon/modules/AACCrash.java
@@ -3,8 +3,8 @@ package com.example.addon.modules;
 import com.example.addon.Addon;
 import meteordevelopment.meteorclient.systems.modules.Module;
 
-public class ModuleExample1 extends Module {
-    public ModuleExample1() {
+public class AACCrash extends Module {
+    public AACCrash() {
         super(Addon.CATEGORY, "AAC Crash", "Supposed crash methrods for servers using AAC");
     }
 }
